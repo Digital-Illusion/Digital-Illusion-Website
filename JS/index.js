3ab40c70ed1b93a1560a008e5c057069353fcc2c
BILLY K <93292710+consciousdeveloper@users.noreply.github.com>
2021-11-03 17:46:20 +0300

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,5 @@
 var Name = document.getElementById('OS');
+var userAgent = navigator.userAgent.toLowerCase();
 if (navigator.appVersion.indexOf("Win") != -1)
     Name.innerHTML = "Windows OS";
 if (navigator.appVersion.indexOf("Mac") != -1)
@@ -7,5 +8,5 @@ if (navigator.appVersion.indexOf("X11") != -1)
     Name.innerHTML = "Arch Linux";
 if (navigator.appVersion.indexOf("Linux") != -1)
     Name.innerHTML = "Linux OS";
-if (navigator.appVersion.indexOf("android") != -1)
+if (navigator.userAgent.indexOf("android") != -1)
     Name.innerHTML = "Android OS";
