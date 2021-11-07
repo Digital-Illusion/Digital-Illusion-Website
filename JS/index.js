077444c8c47dc1f53ee99e5337a7b4532d703b41
George Muigai <87810398+GeorgeMuigai@users.noreply.github.com>
2021-11-07 09:51:23 +0300

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,13 +1,15 @@
-var Name = document.getElementById('OS');
-var userAgent = navigator.userAgent.toLowerCase();
-var Android = userAgent.indexOf("android") != -1;
-if (navigator.appVersion.indexOf("Win") != -1)
-    Name.innerHTML = "Windows OS";
-if (navigator.appVersion.indexOf("Mac") != -1)
-    Name.innerHTML = "Mac OS";
-if (navigator.appVersion.indexOf("X11") != -1)
-    Name.innerHTML = "Arch Linux";
-if (navigator.appVersion.indexOf("Linux") != -1)
-    Name.innerHTML = "Linux OS";
-if (Android)
-    Name.innerHTML = "Android OS";
+var up = this.document.getElementById('up');
+window.addEventListener('scroll', function(){
+  if(this.window.scrollY > 0)
+  {
+    up.style.visibility = "visible";
+    up.style.bottom = "10%";
+  }else
+  {
+    up.style.visibility = "hidden";
+    up.style.bottom = "100%";
+  }
+})
+up.addEventListener('click', function(){
+  window.scrollTo(0, 0);
+})
